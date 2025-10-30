04a0b9938886ac45abafbce072a5254712c5cfde
Mishambrick <mishelgolub@gmail.com>
2025-10-30 21:44:51 +0300

    ну я тут парсер на коленках собрал

--- mishanya.py
+++ mishanya.py
@@ -1,10 +1,1 @@
-def mixing():
-    temp = 0
-    for i in range(10000):
-        temp +=1
-    print("Ёмаё у миксера перегрев, ой че же делать то")
-
-if input('Хотите запустить миксер? Y/N: ') == 'Y':
-    mixing()
-else:
-    print("бр бр патапим")
+print('parser')
