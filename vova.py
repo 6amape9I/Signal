4e1f01d8d5a439d87b47563c3e556073a31ebbd2
Maya-little-bee <majabelina3@xmail.ru>
2025-12-16 14:44:14 +0300

    Update vova.py

--- vova.py
+++ vova.py
@@ -1,2 +1,10 @@
-for i in range(int(input())):
-    print('')
+def mixing():
+    temp = 0
+    for i in range(10000):
+        temp +=1
+    print("Ёмаё у миксера перегрев, ой че же делать то")
+
+if input('Хотите запустить миксер? Y/N: ') == 'Y':
+    mixing()
+else:
+    print("бр бр патапим")
