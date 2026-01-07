7b423c53bdc12ce7d935e4e7f897c81debb84606
Vovandrik <ivanov.va.krs@gmail.com>
2026-01-07 11:32:49 +0300

    Delete 12.py

--- vova/vova/12.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def GetNews(page = 0):
-    news = requests.get(f'https://www.vesti.ru/api/news?page={page}')
-    article = news.json()['data'][0]['id']
-    oneNews = requests.get(f'https://www.vesti.ru/api/article/{article}')
-    print(oneNews.json()['data']['body'])
-
-GetNews()
