e3f8988139d57fdcdf6bce7afe803ae115c29498
Vovandrik <ivanov.va.krs@gmail.com>
2026-01-07 11:36:52 +0300

    Update pyrser.py

--- vova/vova/питер парсер/pyrser.py
+++ vova/vova/питер парсер/pyrser.py
@@ -1,9 +1,11 @@
 import requests
 
-def GetNews(page = 0):
+def GetNews(page):
     news = requests.get(f'https://www.vesti.ru/api/news?page={page}')
     article = news.json()['data'][0]['id']
     oneNews = requests.get(f'https://www.vesti.ru/api/article/{article}')
     print(oneNews.json()['data']['body'])
 
-GetNews()
+for i in range(1,10):
+    GetNews(i)
+    print('sled str')
